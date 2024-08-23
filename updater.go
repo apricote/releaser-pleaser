e88dbcaf85d050a470676906b890fab71d25fb81
Julian Tölle <julian.toelle97@gmail.com>
2024-08-23 21:56:52 +0200

    refactor: move changelog file to updater interface

--- updater.go
+++ updater.go
@@ -1,12 +1,14 @@
 package rp
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
 
 var (
 	GenericUpdaterSemVerRegex   = regexp.MustCompile(`\d+\.\d+\.\d+(-[\w.]+)?(.*x-releaser-pleaser-version)`)
+	ChangelogUpdaterHeaderRegex = regexp.MustCompile(`^# Changelog\n`)
 )
 
 type ReleaseInfo struct {
@@ -25,4 +27,21 @@ func (u *GenericUpdater) UpdateContent(content string, info ReleaseInfo) (string
 	version := strings.TrimPrefix(info.Version, "v")
 
 	return GenericUpdaterSemVerRegex.ReplaceAllString(content, version+"${2}"), nil
+}
+
+type ChangelogUpdater struct{}
+
+func (u *ChangelogUpdater) UpdateContent(content string, info ReleaseInfo) (string, error) {
+	headerIndex := ChangelogUpdaterHeaderRegex.FindStringIndex(content)
+	if headerIndex == nil && len(content) != 0 {
+		return "", fmt.Errorf("unexpected format of CHANGELOG.md, header does not match")
+	}
+	if headerIndex != nil {
+		// Remove the header from the content
+		content = content[headerIndex[1]:]
+	}
+
+	content = ChangelogHeader + "\n\n" + info.ChangelogEntry + content
+
+	return content, nil
 }
