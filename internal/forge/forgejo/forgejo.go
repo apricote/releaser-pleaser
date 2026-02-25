784cf1165f3b52f9819b6f32e053846f3d7c7643
Ben Vidulich <ben@vidulich.nz>
2026-02-26 05:23:24 +1100

    fix(forgejo): locate PRs by index (#326)
    When opening a new or existing pull request against a Forgejo repository, the following error occurs:
    
        ERR failed to reconcile release pull request: The target couldn't be found.

--- internal/forge/forgejo/forgejo.go
+++ internal/forge/forgejo/forgejo.go
@@ -279,7 +279,7 @@ func (f *Forgejo) CreatePullRequest(ctx context.Context, pr *releasepr.ReleasePu
 	}
 
 	// TODO: String ID?
-	pr.ID = fPR.ID
+	pr.ID = fPR.Index
 
 	err = f.SetPullRequestLabels(ctx, pr, []releasepr.Label{}, pr.Labels)
 	if err != nil {
@@ -452,7 +452,7 @@ func all[T any](f func(listOptions forgejo.ListOptions) ([]T, *forgejo.Response,
 
 func forgejoPRToPullRequest(pr *forgejo.PullRequest) *git.PullRequest {
 	return &git.PullRequest{
-		ID:          pr.ID,
+		ID:          pr.Index,
 		Title:       pr.Title,
 		Description: pr.Body,
 	}
